a0abeca3fd8b95442cfb948334eb91e642744d39
Jesús Pérez <jesusprubio@gmail.com>
2017-05-17 01:19:12 +0200

    Requiring stuff dinamically and other minor improvements.

--- index.js
+++ index.js
@@ -7,19 +7,11 @@
 
 'use strict';
 
-const address = require('address');
-const icanhazip = require('icanhazip');
-const logger = require('pown-logger');
-
-let pkgName = require('./package').name;
-
-
 const defaults = false;
-pkgName = pkgName.slice(5);
 
 
 exports.yargs = {
-  command: pkgName,
+  command: 'ip',
   // TODO: Improve the engine to get it from the package.json
   // to avoid to repeat the same in multiple places.
   describe: 'Get info for a network interface: IPv4,' +
@@ -39,10 +31,17 @@ exports.yargs = {
   },
 
   handler: (argv = {}) => {
-    logger.title(pkgName);
+    /* eslint-disable global-require */
+    const address = require('address');
+    const icanhazip = require('icanhazip');
+    const logger = require('pown-logger');
+    /* eslint-enable global-require */
+
+
+    logger.title(this.yargs.command);
 
     address(argv.interface, (err, addrs) => {
-      if (err) throw err;
+        if (err) throw err;
 
       logger.result('Addresses', addrs);
 
